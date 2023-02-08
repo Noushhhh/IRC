0719a8749fb713c53a2c7489efbfb5368b30f4aa
Noushhhh <ana.andric@live.fr>
2023-02-08 17:38:40 +0100

    feat(Message): implemented class Message with list of functions to parse messages and potentially execute

--- includes/Message.hpp
+++ includes/Message.hpp
@@ -15,21 +15,79 @@
 
 #include "irc.hpp"
 
+#define HANDLEDCOMMANDS "{ "PASS", "NICK", "USER", "QUIT", "JOIN", "PART", "MODE", "TOPIC", "NAMES", "LIST", "INVITE", "KICK", "MSG", "PRIVMSG", "NOTICE", "PING", "PONG" }"
+#define HANDLEDCOMMANDSNB 17
+
 class Message
 {
-private	:
+public	:
 
-
+	std::string						_message;
+	bool							_isRequest;
+	std::string						_handledCommands[HANDLEDCOMMANDSNB];
+	void							(Message::*_ptrF[HANDLEDCOMMANDSNB])(std::string command, std::string parameters);
+	// std::string						*_commands;
+	// std::string						*_parameters;
 
 public	:
 
 	Message();
+	Message(std::string message);
 	Message(const Message &src);
 	~Message();
 	
 	Message &operator=(const Message &src);
+
+	bool	isRequest();
+	bool	parseMessage();
+
+	void	Pass(std::string command, std::string parameters);
+	void	Nick(std::string command, std::string parameters);
+	void	User(std::string command, std::string parameters);
+	void	Quit(std::string command, std::string parameters);
+	void	Join(std::string command, std::string parameters);
+	void	Part(std::string command, std::string parameters);
+	void	Mode(std::string command, std::string parameters);
+	void	Topic(std::string command, std::string parameters);
+	void	Names(std::string command, std::string parameters);
+	void	List(std::string command, std::string parameters);
+	void	Invite(std::string command, std::string parameters);
+	void	Kick(std::string command, std::string parameters);
+	void	Msg(std::string command, std::string parameters);
+	void	Privmsg(std::string command, std::string parameters);
+	void	Notice(std::string command, std::string parameters);
+	void	Ping(std::string command, std::string parameters);
+	void	Pong(std::string command, std::string parameters);
+
 };
 
+// USER
+
+// PASS
+// NICK
+// USER
+// QUIT
+// 
+
+// CHANNEL
+// JOIN
+// PART
+// MODE :  O - give "channel creator" status;
+        // o - give/take channel operator privilege;
+        // v - give/take the voice privilege; + si affinite mais pas oblige
+// TOPIC
+// NAMES
+// LIST
+// INVITE
+// KICK
+
+// MSG
+// PRIVMSG
+// NOTICE - same as privmsg pour les bots (a bot never receives message when calling notice)
+
+// MISCELLANEOUS
+// PING
+// PONG
 
 
 #endif
--- src/Message.cpp
+++ src/Message.cpp
@@ -14,14 +14,91 @@
 #include "../includes/Message.hpp"
 
 
-Message::Message()
+Message::Message() : _message(""), // init members
 {
+
+	this->_ptrF[0] = (&Message::Pass);
+	this->_ptrF[1] = (&Message::Nick);
+	this->_ptrF[2] = (&Message::User);
+	this->_ptrF[3] = (&Message::Quit);
+	this->_ptrF[4] = (&Message::Join);
+	this->_ptrF[5] = (&Message::Part);
+	this->_ptrF[6] = (&Message::Mode);
+	this->_ptrF[7] = (&Message::Topic);
+	this->_ptrF[8] = (&Message::Names);
+	this->_ptrF[9] = (&Message::List);
+	this->_ptrF[10] = (&Message::Invite);
+	this->_ptrF[11] = (&Message::Kick);
+	this->_ptrF[12] = (&Message::Msg);
+	this->_ptrF[13] = (&Message::Privmsg);
+	this->_ptrF[14] = (&Message::Notice);
+	this->_ptrF[15] = (&Message::Ping);
+	this->_ptrF[16] = (&Message::Pong);
+
+	this->_handledCommands[0] = "PASS";
+	this->_handledCommands[1] = "NICK";
+	this->_handledCommands[2] = "USER";
+	this->_handledCommands[3] = "QUIT";
+	this->_handledCommands[4] = "JOIN";
+	this->_handledCommands[5] = "PART";
+	this->_handledCommands[6] = "MODE";
+	this->_handledCommands[7] = "TOPIC";
+	this->_handledCommands[8] = "NAMES";
+	this->_handledCommands[9] = "LIST";
+	this->_handledCommands[10] = "INVITE";
+	this->_handledCommands[11] = "KICK";
+	this->_handledCommands[12] = "MSG";
+	this->_handledCommands[13] = "PRIVMSG";
+	this->_handledCommands[14] = "NOTICE";
+	this->_handledCommands[15] = "PING";
+	this->_handledCommands[16] = "PONG";
+
 	// std::cerr << "Debug message: Message Default Constructor called" << std::endl;
 }
 
+Message::Message(std::string message) : _message(message)
+{
+	this->_ptrF[0] = (&Message::Pass);
+	this->_ptrF[1] = (&Message::Nick);
+	this->_ptrF[2] = (&Message::User);
+	this->_ptrF[3] = (&Message::Quit);
+	this->_ptrF[4] = (&Message::Join);
+	this->_ptrF[5] = (&Message::Part);
+	this->_ptrF[6] = (&Message::Mode);
+	this->_ptrF[7] = (&Message::Topic);
+	this->_ptrF[8] = (&Message::Names);
+	this->_ptrF[9] = (&Message::List);
+	this->_ptrF[10] = (&Message::Invite);
+	this->_ptrF[11] = (&Message::Kick);
+	this->_ptrF[12] = (&Message::Msg);
+	this->_ptrF[13] = (&Message::Privmsg);
+	this->_ptrF[14] = (&Message::Notice);
+	this->_ptrF[15] = (&Message::Ping);
+	this->_ptrF[16] = (&Message::Pong);
+
+	this->_handledCommands[0] = "PASS";
+	this->_handledCommands[1] = "NICK";
+	this->_handledCommands[2] = "USER";
+	this->_handledCommands[3] = "QUIT";
+	this->_handledCommands[4] = "JOIN";
+	this->_handledCommands[5] = "PART";
+	this->_handledCommands[6] = "MODE";
+	this->_handledCommands[7] = "TOPIC";
+	this->_handledCommands[8] = "NAMES";
+	this->_handledCommands[9] = "LIST";
+	this->_handledCommands[10] = "INVITE";
+	this->_handledCommands[11] = "KICK";
+	this->_handledCommands[12] = "MSG";
+	this->_handledCommands[13] = "PRIVMSG";
+	this->_handledCommands[14] = "NOTICE";
+	this->_handledCommands[15] = "PING";
+	this->_handledCommands[16] = "PONG";
+
+} // init members
+
 Message::Message(const Message &src)
 {
-	*this = classInstance;
+	*this = src;
 	// std::cerr << "Debug message: Message Default Copy Constructor called" << std::endl;
 }
 
@@ -30,7 +107,80 @@ Message::~Message()
 	// std::cerr << "Debug message: Message Default Destructor called" << std::endl;
 }
 
-Classename &Message::operator=(const Message &src)
+Message &Message::operator=(const Message &src)
 {
-	
+
 }
+
+bool Message::isRequest()
+{
+
+
+}
+
+bool Message::parseMessage()
+{
+	std::string		*split_message;
+	std::string		token;
+	int				it = 0;
+	int				pos = 0;
+
+	if (!_message) 
+	{
+		std::cout << "not a request." << std::endl;
+		return false;
+	}
+
+	while (pos != _message.length())
+	{
+		token = _message.substr(pos, _message.find(" "));
+		split_message[it] = token;
+		pos = _message.find(" ");
+		it++;
+	}
+
+	int it1 = 0;
+	std::string parameters;
+	while (split_message[it1] != 0) // std::empty ?
+	{
+		for (int it2 = 0; it2 < HANDLEDCOMMANDSNB; it2++)
+		{
+			if (split_message[it1] == _handledCommands[it2])
+			{
+				for (int it2 = 0; i < HANDLEDCOMMANDSNB; i++)
+				{
+					if (split_message[it1] == _handledCommands[it1])
+					{
+						
+					}
+					i++;
+				}
+				(void)(this->*_ptrF[i])())
+			}
+			it1++;
+		}
+	}
+	// for (int i = 0; i < it; i++)
+	// {
+	// 	if ()
+	// }
+}
+
+
+// void	Pass(std::string command, std::string parameters);
+// void	Nick(std::string command, std::string parameters);
+// void	User(std::string command, std::string parameters);
+// void	Quit(std::string command, std::string parameters);
+// void	Join(std::string command, std::string parameters);
+// void	Part(std::string command, std::string parameters);
+// void	Mode(std::string command, std::string parameters);
+// void	Topic(std::string command, std::string parameters);
+// void	Names(std::string command, std::string parameters);
+// void	List(std::string command, std::string parameters);
+// void	Invite(std::string command, std::string parameters);
+// void	Kick(std::string command, std::string parameters);
+// void	Msg(std::string command, std::string parameters);
+// void	Privmsg(std::string command, std::string parameters);
+// void	Notice(std::string command, std::string parameters);
+// void	Ping(std::string command, std::string parameters);
+// void	Pong(std::string command, std::string parameters);
