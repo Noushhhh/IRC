2fec0f54c5a2add1a841e98fd883c690c8421c54
Noushhhh <ana.andric@live.fr>
2023-02-14 15:36:36 +0100

    before pull master

--- src/Server.cpp
+++ src/Server.cpp
@@ -418,7 +418,11 @@ bool	Server::Nick(User &user, Message &message)
 		send(user.getSockfd(), "ERR_NONICKNAMEGIVEN", 19, 0); 
 		return false;
 	}
-	
+	std::string nickname = *message._splitMessage.(begin() + 1);
+	for (_usersListIt = _usersList.begin(); _usersListIt != _usersList.end(); _usersListIt++)
+	{
+		if (_usersListIt->getNickname() == message._splitMessage.begin() + 1)
+	}
 	// ERR_ERRONEUSNICKNAME
 	// std::list<User>::const_iterator it;
 	// it = std::find()
