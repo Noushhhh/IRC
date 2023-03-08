17de01fe8d4c4182feb78c2637ab2fb675d7a4e6
Noushhhh <ana.andric@live.fr>
2023-03-08 13:26:08 +0100

    style(Pass): with message._arguments and message._cmd, cleared up

--- src/cmds/pass.cpp
+++ src/cmds/pass.cpp
@@ -6,7 +6,7 @@
 /*   By: aandric <aandric@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:58:16 by mgolinva          #+#    #+#             */
-/*   Updated: 2023/03/03 15:39:07 by aandric          ###   ########.fr       */
+/*   Updated: 2023/03/08 13:25:27 by aandric          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,36 @@ void	Server::Pass(User &user, Message &message)
 {
 	if (message._argsNb < 2)
 	{
-		_errMsg = ERR_NEEDMOREPARAMS(message._splitMessage[0]);
+		_errMsg = ERR_NEEDMOREPARAMS(message._cmd);
 		send(user.getSockfd(), _errMsg.c_str(), _errMsg.length(), 0);
 		return ;
 	}
-	if (user.getPassword().empty())
-	{
-		message._it = message._splitMessage.begin() + 1;
-		user.setPassword(*message._it);
-		return ;
-	}
-	else
+	if (user.getRegistered())
 	{
 		_errMsg = ERR_ALREADYREGISTERED;
 		send(user.getSockfd(), _errMsg.c_str(), _errMsg.length(), 0);
 		return ;
 	}
-	return ;
+	else
+		user.setPassword(message._arguments[0]);
 }
+
+// PASS message
+//      Command: PASS
+//   Parameters: <password>
+// The PASS command is used to set a ‘connection password’. If set, the password must be set before any attempt to register the connection is made. This requires that clients send a PASS command before sending the NICK / USER combination.
+
+// The password supplied must match the one defined in the server configuration. It is possible to send multiple PASS commands before registering but only the last one sent is used for verification and it may not be changed once the client has been registered.
+
+// If the password supplied does not match the password expected by the server, then the server SHOULD send ERR_PASSWDMISMATCH (464) and MAY then close the connection with ERROR. Servers MUST send at least one of these two messages.
+
+// Servers may also consider requiring SASL authentication upon connection as an alternative to this, when more information or an alternate form of identity verification is desired.
+
+// Numeric replies:
+
+// ERR_NEEDMOREPARAMS (461)
+// ERR_ALREADYREGISTERED (462)
+// ERR_PASSWDMISMATCH (464)
+// Command Example:
+
+//   PASS secretpasswordhere
